6ba1a90de03e4364d356556cf3c94af4d0fa9bbd
Student <student11@ip-172-31-23-20.eu-west-3.compute.internal>
2020-02-06 22:00:45 +0000

    Added task 3c).

--- homework_1.sh
+++ homework_1.sh
@@ -3,17 +3,14 @@
 # grep finds all "chrII" in the given file. cut -f 3 take out column 3  on the lines where "chrII" occurs
 # and sort direct column 3 to a given file.
 #  wc -l couts the number of lines where "gene" occurs on.
-
-grep "chrII" data2/saccharomyces_cerevisiae_R64-2-1_20150113.gff | cut -f 3 | sort > homeworks/group_chrII.txt
-grep "gene" homeworks/group_chrII.txt | wc -l
-
+grep "chrII" data2/saccharomyces_cerevisiae_R64-2-1_20150113.gff | cut -f 3 | sort > homeworks/group_chrII.txtgrep "gene" homeworks/group_chrII.txt | wc -l
 #The answer i got was 681
 
 # 2) Count GC content.
-# First sort out only the bases from the file. 
+# First sort out only the bases from the file, using grep -v that returns the file without the lines that contain the matching sequence.
 grep -v ">ref" data2/S288C_reference_sequence_R64-2-1_20150113.fsa | sort > homeworks/hw1_ex2_bases
-# grep -o finds only the matching parts in the file and return each matching string,
-# either A,T,G or C, on a separate line that are either A,T,C or G. 
+# grep -o finds only the matching parts in the file and return each matching string, on separate lines.
+# in this case, either A,T,G or C.
 grep -o "A\|T\|C\|G" homeworks/hw1_ex2_bases | wc -l
 # returns: 12157105
 # counts C's and G's in the same way as above
@@ -30,6 +27,7 @@ grep -v ">" Y55_JRIF00000000_SGD_cds.fsa |grep -o "C\|G" | wc -l
 grep -v ">" Y55_JRIF00000000_SGD_cds.fsa |grep -o "A\|T\|G\|C" | wc -l
 # returns: 8073830
 # Using bc the percentage of GC content is calculated to 0.39534335, very close to the value in 2).
+
 #3b)
 # Counting all lines with ">" in the  reference genome, this will be equal to all ORFs.
 grep ">" data2/orf_coding_all_R64-2-1_20150113.fasta | wc -l
@@ -37,3 +35,13 @@ grep ">" data2/orf_coding_all_R64-2-1_20150113.fasta | wc -l
 # Counting all lines with ">" in the Y55 genome, this will be equal to number of ORFs.
 grep ">" Y55_JRIF00000000_SGD_cds.fsa | wc -l
 # returns: 5330
+
+#3c)
+# Counting the number of common ORFs between Y55 and the reference.
+# sed "s/ .*//" replaces " .*" with "". Meaning that Everything from " " and forward replaces by nothing. All genes named UNDEF will also be ignored. 
+# This result in that only the gene identifier will be saved in the result file.
+grep ">" data2/orf_coding_all_R64-2-1_20150113.fasta | sed "s/ .*//" | grep -v ">UNDEF" | sort > homeworks/hw1_3C_genes_reference
+grep ">" Y55_JRIF00000000_SGD_cds.fsa | sed "s/_.*//" | grep -v ">UNDEF" | sort > homeworks/hw1_3C_genes_Y55
+# comm -12 returns all lines that file_1 and file_2 have in common. And these lines are then counted, giving the number of commonORFs.
+comm -12 homeworks/hw1_3C_genes_reference homeworks/hw1_3C_genes_Y55 | wc -l
+# returns: 5168
